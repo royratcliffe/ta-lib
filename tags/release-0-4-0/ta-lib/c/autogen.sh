77ae24fb6f7d2af12603d60575be982cadd5c688
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-05-29 18:36:33 +0100

    Builds on OS X 10.7 using Xcode 4.3.2 tools
    Make sure you deactivate MacPorts using "sudo port deactivate active" to avoid libtool confusion.

--- tags/release-0-4-0/ta-lib/c/autogen.sh
+++ tags/release-0-4-0/ta-lib/c/autogen.sh
@@ -3,8 +3,8 @@ echo aclocal
 aclocal || exit
 echo autoheader
 autoheader || exit
-echo libtoolize --copy --force
-libtoolize --copy --force || exit
+echo glibtoolize --copy --force
+glibtoolize --copy --force || exit
 echo automake -a -c
 automake -a -c || exit
 echo autoconf
